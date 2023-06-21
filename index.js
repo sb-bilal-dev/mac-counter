b2e1a0df74387ad637e549c44c9b09534de780e7
sb-bilal-dev <saidbilol18@gmail.com>
2023-06-22 02:07:14 +0500

    add delete all request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("Hello Xanavi");
 
+const { error } = require("console");
 const express = require("express");
 const fs = require("fs");
 const app = express();
@@ -36,30 +37,41 @@ app.get("/", (req, res) => {
 
     if (automobiles[model].includes(mac_address)) {
         res.status(202).send(`Mac Address: ${mac_address} for model: ${model} already exist`)
+    } else {
+        automobiles[model].push(mac_address);
+        var quantity = automobiles[model].length;
+    
+        // Write the updated data to the JSON file
+        try {
+          fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(automobiles, null, 2), "utf8");
+        } catch (err) {
+          console.error("Failed to write data to the file:", err);
+          res.status(500).send("Failed to store the mac_address.\n");
+          return;
+        }
+    
+        // Send a response indicating successful storage
+        var responseBody = `Successfully stored mac_address "${mac_address}", #${quantity} for model "${model}".\n`
+        res
+          .status(200)
+          .send(responseBody);
+          console.log(responseBody)    
     }
-    automobiles[model].push(mac_address);
-    var quantity = automobiles[model].length;
-
-    // Write the updated data to the JSON file
-    try {
-      fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(automobiles, null, 2), "utf8");
-    } catch (err) {
-      console.error("Failed to write data to the file:", err);
-      res.status(500).send("Failed to store the mac_address.\n");
-      return;
-    }
-
-    // Send a response indicating successful storage
-    var responseBody = `Successfully stored mac_address "${mac_address}", #${quantity} for model "${model}".\n`
-    res
-      .status(200)
-      .send(responseBody);
-      console.log(responseBody)
   } else {
     // Send an error response for invalid requests
     res.status(400).send("Invalid request.\n");
   }
 });
+
+app.get("/delete_all", (_, res) => {
+    try {
+        fs.writeFileSync(DATA_FILE_PATH, JSON.stringify({}));
+        res.status(200).send("SUCCESS, all data cleared")        
+    } catch(err) {
+        console.error(err)
+        res.status(500).send("Unknown error occured: ", error)
+    }
+})
 
 // Start the server on port 3000
 app.listen(3000, () => {
