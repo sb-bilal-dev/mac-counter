b894930e4ccb24444b7538cb61cda81f426a4bb7
sb-bilal-dev <saidbilol18@gmail.com>
2023-06-22 02:24:57 +0500

    add db

--- index.js
+++ index.js
@@ -1,63 +1,73 @@
 console.log("Hello Xanavi");
 
-const { error } = require("console");
 const express = require("express");
-const fs = require("fs");
 const app = express();
+const { MongoClient } = require("mongodb");
 
-// Path to the JSON file for storing the data
-const DATA_FILE_PATH = "automobiles.json";
+// Connection URI and database name
+const mongoURI = "mongodb://localhost:27017"; // Replace with your MongoDB connection URI
+const dbName = "your-database-name"; // Replace with your database name
 
-// Read the existing data from the JSON file
-let automobiles = initDB();
+// Initialize MongoDB client and connect to the database
+const client = new MongoClient(mongoURI);
 
-function initDB() {
-    try {
-    if (!fs.existsSync(DATA_FILE_PATH)) {
-        fs.writeFileSync(DATA_FILE_PATH, JSON.stringify({}));
-    }
-    
-    const data = fs.readFileSync(DATA_FILE_PATH, "utf8");
-    return JSON.parse(data);
-    } catch (err) {
-      console.error("Failed to read data from the file:", err);
-      return {}
-    }
-}
+// Middleware to ensure the database connection is established before handling requests
+app.use((req, res, next) => {
+  if (!client.isConnected()) {
+    client.connect()
+      .then(() => {
+        req.db = client.db(dbName);
+        next();
+      })
+      .catch((err) => {
+        console.error("Failed to connect to MongoDB:", err);
+        res.status(500).send("Failed to connect to MongoDB.\n");
+      });
+  } else {
+    req.db = client.db(dbName);
+    next();
+  }
+});
 
 // Configure the route to handle GET requests
 app.get("/", (req, res) => {
   const { model, mac_address } = req.query;
 
   if (model && mac_address) {
-    // Store the mac_address in the automobiles object
-    if (!automobiles[model]) {
-      automobiles[model] = [];
-    }
+    const collection = req.db.collection("automobiles");
 
-    if (automobiles[model].includes(mac_address)) {
-        console.log("Duplicate mac received: ", mac_address)
-        res.status(202).send(`Mac Address: ${mac_address} for model: ${model} already exist`)
-    } else {
-        automobiles[model].push(mac_address);
-        var quantity = automobiles[model].length;
-    
-        // Write the updated data to the JSON file
-        try {
-          fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(automobiles, null, 2), "utf8");
-        } catch (err) {
-          console.error("Failed to write data to the file:", err);
-          res.status(500).send("Failed to store the mac_address.\n");
-          return;
+    // Check if the mac_address already exists for the model
+    collection.findOne({ model, mac_address })
+      .then((existingData) => {
+        if (existingData) {
+          console.log("Duplicate mac received: ", mac_address);
+          res.status(202).send(`Mac Address: ${mac_address} for model: ${model} already exists.`);
+        } else {
+          // Store the mac_address in the database
+          collection.insertOne({ model, mac_address })
+            .then(() => {
+              // Send a response indicating successful storage
+              collection.countDocuments({ model })
+                .then((quantity) => {
+                  var responseBody = `Successfully stored mac_address "${mac_address}", #${quantity} for model "${model}".\n`;
+                  res.status(200).send(responseBody);
+                  console.log(responseBody);
+                })
+                .catch((err) => {
+                  console.error("Failed to count documents:", err);
+                  res.status(500).send("Failed to store the mac_address.\n");
+                });
+            })
+            .catch((err) => {
+              console.error("Failed to insert document:", err);
+              res.status(500).send("Failed to store the mac_address.\n");
+            });
         }
-    
-        // Send a response indicating successful storage
-        var responseBody = `Successfully stored mac_address "${mac_address}", #${quantity} for model "${model}".\n`
-        res
-          .status(200)
-          .send(responseBody);
-          console.log(responseBody)    
-    }
+      })
+      .catch((err) => {
+        console.error("Failed to find existing data:", err);
+        res.status(500).send("Failed to store the mac_address.\n");
+      });
   } else {
     // Send an error response for invalid requests
     res.status(400).send("Invalid request.\n");
@@ -65,25 +75,38 @@ app.get("/", (req, res) => {
 });
 
 app.get("/count", (req, res) => {
-    const { model } = req.query;
-    if (!model) {
-        res.status(400).send("Provide model")
-    } else {
-        var quantity = automobiles[model].length;
+  const { model } = req.query;
+  
+  if (model) {
+    const collection = req.db.collection("automobiles");
 
-        res.status(200).send(quantity)
-    }
-})
+    // Count the number of mac_addresses for the model
+    collection.countDocuments({ model })
+      .then((quantity) => {
+        res.status(200).send(quantity.toString());
+      })
+      .catch((err) => {
+        console.error("Failed to count documents:", err);
+        res.status(500).send("Failed to retrieve the count.\n");
+      });
+  } else {
+    res.status(400).send("Provide model.\n");
+  }
+});
 
 app.get("/delete_all", (_, res) => {
-    try {
-        fs.writeFileSync(DATA_FILE_PATH, JSON.stringify({}));
-        res.status(200).send("SUCCESS, all data cleared")        
-    } catch(err) {
-        console.error(err)
-        res.status(500).send("Unknown error occured: ", error)
-    }
-})
+  const collection = req.db.collection("automobiles");
+
+  // Delete all documents from the collection
+  collection.deleteMany({})
+    .then(() => {
+      res.status(200).send("SUCCESS, all data cleared");
+    })
+    .catch((err) => {
+      console.error("Failed to delete documents:", err);
+      res.status(500).send("Failed to clear data.\n");
+    });
+});
 
 // Start the server on port 3000
 app.listen(3000, () => {
